a932d30530e15e519b2db78f518057ccff7d9667
oukanbun <30286782+oukanbun@users.noreply.github.com>
2017-08-15 10:38:56 +0900

    Add files via upload

---
+++ myjsondata.js
@@ -0,0 +1,58 @@
+var myJsonData = [
+{
+	"id": "1",
+	"latitude": "35.698353",
+	"longitude": "139.773114",
+	"description": "普通",
+	"altitude": "10.0",
+	"name": "秋葉原駅"
+}, 
+{
+	"id": "2",
+	"latitude": "35.681167",
+	"longitude": "139.767052",
+	"description": "混んでいる",
+	"altitude": "10.0",
+	"name": "東京駅"
+},
+{
+	"id": "3",
+	"latitude": "35.689407",
+	"longitude": "139.700306",
+	"description": "普通",
+	"altitude": "10.0",
+	"name": "新宿駅"
+},
+{
+	"id": "4",
+	"latitude": "35.729503",
+	"longitude": "139.7109",
+	"description": "普通",
+	"altitude": "10.0",
+	"name": "池袋駅"
+},
+{
+	"id": "5",
+	"latitude": "35.610243",
+	"longitude": "139.750374",
+	"description": "普通",
+	"altitude": "10.0",
+	"name": "品川駅"
+},
+{
+	"id": "6",
+	"latitude": "35.712364",
+	"longitude": "139.776188",
+	"description": "普通",
+	"altitude": "10.0",
+	"name": "上野駅"
+},
+{
+	"id": "7",
+	"latitude": "35.658034",
+	"longitude": "139.701636",
+	"description": "混んでいる",
+	"altitude": "10.0",
+	"name": "渋谷駅"
+},
+];
